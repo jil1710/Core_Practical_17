2d19478aeb67a3fbf1e688ed1ef62217930c37ee
rider <jil.p@simformsolutions.com>
2023-07-07 16:20:42 +0530

    add: authentication and role base authorization scheme

---
+++ Core_Practical_17/ViewModel/Login.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core_Practical_17.ViewModel
+{
+    public class Login
+    {
+
+        
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public required string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
+
+    }
+}
